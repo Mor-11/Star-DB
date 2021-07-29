5514f479fcd490d4b91b70b367c9101f3c309060
Maryna Zhuchyk <marinazh122000@gmail.com>
2021-07-29 10:31:32 +0300

    feat: add components

--- start-db/src/index.js
+++ start-db/src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+export default class SwapiService {
+  _apiBase = "https://swapi.dev/api/";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url} = , received ${res.status}`);
+    }
+    const body = await res.json();
+    return body;
+  }
+
+  async getAllPeople() {
+    const res = await this.getResource("/people/");
+    return res.results;
+  }
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`);
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource("/planets/");
+    return res.results;
+  }
+
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}/`);
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource("/starships/");
+    return res.results;
+  }
+
+  getStarships(id) {
+    return this.getResource(`/starships/${id}/`);
+  }
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllPeople().then((people) => {
+  people.forEach((p) => {
+    console.log(p.name);
+  });
+});
